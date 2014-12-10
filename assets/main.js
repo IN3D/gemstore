db9df5c345010b97b10fb8eb744f46314788d81c
Eric Hopkins <scarecrow3322@gmail.com>
2014-12-09 23:58:03 -0500

    first part of level 3

--- assets/main.js
+++ assets/main.js
@@ -25,6 +25,18 @@ var gems = [
                 thumb: 'assets/img/gem-03.gif'
             }
         ],
+        reviews: [
+            {
+                stars: 5,
+                body: "I love this product!",
+                author: "joe@thomas.com"
+            },
+            {
+                stars: 1,
+                body: "This product sucks",
+                author: "tim@hater.com"
+            }
+        ],
         canPurchase: true,
         soldOut: true,
     },
